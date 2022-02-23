6b14182ba185165f5cf980ab8568cf5e48c30684
ryuash <ryuash123@gmail.com>
2022-02-23 15:10:20 +0800

    wip im bored

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
-import * as R from 'ramda';
-import express, {
-  Request, Response,
-} from 'express';
+import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { bankClient } from '@grpc/client';
+import { QueryAllBalancesRequest } from '@proto/cosmos/bank/v1beta1/query_pb';
+import { PageRequest } from '@proto/cosmos/base/query/v1beta1/pagination_pb';
 import { AccountBalanceRequestType } from './types';
 
 const app = express();
@@ -19,11 +19,32 @@ app.get('/ping', (_, res) => {
   res.status(200).send('pong');
 });
 
-app.post('/account_balance', (req, res) => {
+app.post('/account_balance', async (req, res) => {
   const body = req.body as AccountBalanceRequestType;
-  console.log(body, 'body');
-  // res.status(200).send('pong');
-  res.status(200).send('pong');
+  const params = new QueryAllBalancesRequest();
+  params.setAddress(body.input.address);
+
+  // const pageRequest = new PageRequest();
+  // pageRequest.setLimit(10);
+  // pageRequest.toObject
+
+  const clientGetBalance = (options: any) => new Promise((resolve, _reject) => {
+    bankClient.allBalances(options, (error, response) => {
+      if (response) {
+        resolve(response.toObject());
+      } else {
+        resolve({
+          balancesList: [],
+        });
+      }
+    });
+  });
+
+  const data = await clientGetBalance(params);
+
+  res.status(200).json({
+    data,
+  });
 });
 
 const init = async () => {
